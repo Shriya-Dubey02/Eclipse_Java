6f8325e43d21c1eda6cd1df310aaba10649cc023
Shriya-Dubey02 <shriyadubey220@gmail.com>
2024-10-13 23:13:56 +0530

    13 October

--- ExceptionHandling/InvalidAgeException.java
+++ ExceptionHandling/InvalidAgeException.java
@@ -4,10 +4,12 @@ package ExceptionHandling;
 public class InvalidAgeException extends RuntimeException {
 	
 	// Every exception has 2 constructors 
+	// Non paramaterised
 	public InvalidAgeException()
 	{
 		super();
 	}
+	// Paramaterised
 	public InvalidAgeException(String message)
 	{
 		super(message);
